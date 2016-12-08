fb1c9385105a7d428a28db83b8d4b9fd7f53cd7f
chrispab <cbattisson@gmail.com>
2016-12-08 22:59:29 +0000

    3 commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,25 @@
-var app= angular.module('store', [ ]);
+(function(){
+    var app= angular.module('store', [ ]);
+
+    app.controller('StoreController',function(){
+        this.products = gems;
+    });
+
+    var gems = [
+        {
+        name: 'Dodecahedron',
+        price:2.95,
+        description: 'what a nice gem it is',
+        canPurchase: true,
+        soldOut: false,
+    },
+    {
+    name: 'Pentagonal Gem',
+    price:5.95,
+    description: 'thast a nice one too',
+    canPurchase: true,
+    soldOut: true,
+    }
+];
+
+})();
